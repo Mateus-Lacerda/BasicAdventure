3d92fa8807849e427f11437d649eea7e3d8e5182
Mateus-Lacerda <mateus.lacerda@discente.ufg.br>
2023-05-18 13:43:24 -0300

    master

--- squeleton.py
+++ squeleton.py
@@ -3,6 +3,6 @@ import random
 
 class squeleton:
     name = "Esqueleto"
-    life = random.randint(20,30)
+    life = random.randint(17,25)
     strength = random.randint(10,15)
     attack_type = "Ossada!"
